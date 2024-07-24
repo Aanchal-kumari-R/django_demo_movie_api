8b7c54fd8e7344d9f0f6bf828aed68e63d6844bc
Aanchal-Kumari-R <aanchalkumari096@gmail.com>
2024-07-24 17:06:33 +0530

    My First Commit

---
+++ mysite/movies/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render 
+from rest_framework import viewsets 
+from .serializers import MovieSerializer 
+from .models import Moviesdata
+
+# Create your views here. 
+
+class MovieViewSet(viewsets.ModelViewSet): 
+    queryset = Moviesdata.objects.all() 
+    serializer_class = MovieSerializer 
+  
+class ActionViewSet(viewsets.ModelViewSet): 
+    queryset =  Moviesdata.objects.filter(typ ="action") 
+    serializer_class = MovieSerializer 
+
+class ComedyViewSet(viewsets.ModelViewSet): 
+    queryset = Moviesdata.objects.filter(typ = "comedy") 
+    serializer_class = MovieSerializer
